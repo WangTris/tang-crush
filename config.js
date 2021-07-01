be0e40ef2c910ef4e37245e32b35c9a58d630c81
Trần Quang Trí <82810134+ladouleurexquise@users.noreply.github.com>
2021-07-01 13:54:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Bae à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'My à!',
+    introDesc: `Vì đời còn lắm sóng gió.
+    Sao em không ngồi lại cùng chút bình minh.`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Tạm bỏ lại gánh trên vai',
+    desc: 'Theo anh đi tìm lại một phút yên bình 😙',
+    btnYes: 'Dạ, Anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    question:'Ngày rồi ngày vẫn những khó khăn. Em vẫn xoay vần cùng những nốt thăng trầm. Nhưng cứ vui lên em',
+    btnReply: 'My love <3',
+    reply: 'Vì ngày mai lại một cơ hội để ôm trọn thế giới <33333333',
+    mess: 'Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Mãi iu em công chúa của anh 🧡',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/tri.tranquang.940' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
